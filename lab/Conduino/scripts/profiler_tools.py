2f56dde226e735ba79f99431c812b35105a57f96
Karina Ramos Musalem <kramosmu@eos.ubc.ca>
2019-08-06 16:16:20 -0700

    added notebook looking at all runs so far

--- lab/Conduino/scripts/profiler_tools.py
+++ lab/Conduino/scripts/profiler_tools.py
@@ -63,3 +63,33 @@ def profiler_19may(time_conduino):
     
     return(dist*100) # m to cm beacuse file is in meters
 
+def profiler_04aug(time_conduino):
+    
+    # Input
+    filename = '/ocean/kramosmu/MultipleCanyons/lab/Conduino/scripts/profiler_05aug19.csv'
+    ini = 0
+
+    # Get timeseries 
+    df = pd.read_csv(filename, delim_whitespace=False, header=1)
+    time_p = np.array(df['t'][:])
+    ydist = np.array(df['y'][:])
+    top_pos = ydist[ini]
+    top_pos2 = ydist[-1]
+    ini_time = time_p[ini]
+    end_time = time_p[-1]
+
+    time = time_p[ini:]-ini_time
+    position = ydist[ini:]-top_pos
+
+    # Find interp function velocity of the profiler
+    time_small = time[0:-1]
+    vel = (position[1:]-position[:-1])/(time[1:]-time[:-1])
+    func = interp1d(time_small, vel)
+    vel_int = func(time_conduino)
+    dist = np.zeros(np.shape(vel_int))
+    
+    for ii in range(1,len(time_conduino)):
+        dist[ii] = (vel_int[ii]*(time_conduino[ii]-time_conduino[ii-1]))+dist[ii-1] 
+    
+    return(dist) # file is in cm
+
