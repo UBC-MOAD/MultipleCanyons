e8a5384de88805a5b5e9928e26c08b36069e49ab
Karina Ramos Musalem <kramosmu@eos.ubc.ca>
2020-01-30 10:41:48 -0800

    Added all untracked notebooks to repo before migration to git

---
+++ lab/Conduino/Calibration/README.md
@@ -0,0 +1,17 @@
+The Jupyter Notebooks in this directory are for development and analysis of 
+results from multiple canyon experiments and instrumentation.
+
+The links below are to static renderings of the notebooks via
+[nbviewer.jupyter.org](http://nbviewer.jupyter.org/).
+Descriptions under the links below are from the first cell of the notebooks
+(if that cell contains Markdown or raw text).
+
+
+##License
+
+These notebooks and files are copyright 2013-2019
+by The University of British Columbia.
+
+They are licensed under the Apache License, Version 2.0.
+http://www.apache.org/licenses/LICENSE-2.0
+Please see the LICENSE file for details of the license.
---
+++ lab/Conduino/Calibration/notebooks/profiler_tools.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import scipy.io as sio
+from scipy.interpolate import interp1d
+
+def profiler_11jun(time_conduino):
+    
+    # Input
+    filename = '../profiler_tracking_11jun19.txt'
+    ini = 19
+    end = 85
+
+    # Get timeseries 
+    df = pd.read_csv(filename, delim_whitespace=True, header=1)
+    time_p = np.array(df['t'][:])
+    ydist = np.array(df['y'][:])
+    top_pos = ydist[ini]
+    top_pos2 = ydist[end]
+    ini_time = time_p[ini]
+    end_time = time_p[end]
+
+    time = time_p[ini:end+1]-ini_time
+    position = ydist[ini:end+1]-top_pos
+
+    # Find interp function velocity of the profiler
+    time_small = time[0:-1]
+    vel = (position[1:]-position[:-1])/(time[1:]-time[:-1])
+    func = interp1d(time_small, vel)
+    vel_int = func(time_conduino)
+    dist = np.zeros(np.shape(time_small))
+    
+    for ii in range(1,len(vel)):
+        dist[ii] = (vel_int[ii]*(time_small[ii]-time_small[ii-1]))+dist[ii-1] 
+
+    return(dist)
+
